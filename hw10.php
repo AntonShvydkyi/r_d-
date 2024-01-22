52017c2b403cc51df79b9f3b0bc466478bfa86e3
Anton Shvydkyi <tohinson91@gmail.com>
2024-01-22 21:48:55 +0300

    update 2

--- hw10.php
+++ hw10.php
@@ -1,105 +1,59 @@
-<!DOCTYPE html>
-<?php
+create table students
+(
+student_id         integer,
+first_name TEXT,
+last_name  TEXT,
+age        integer not null,
+faculty_id  TEXT
+);
 
-session_start();
+INSERT INTO students (student_id, first_name, last_name, age, faculty_id)
+VALUES
+(1, 'John', 'Doe', '1995-03-15', 1),
+(2, 'Jane', 'Smith', '1998-07-21', 2),
+(3, 'Alex', 'Johnson', '1997-11-30', 1);
 
-if (!empty($_POST['email']) && !empty($_POST['password'])) {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
 
-    $dataFilePass = 'data.txt';
 
-    $delimeter = ', ';
+create table faculties
+(
+faculty_id_id           integer,
+faculty_name TEXT
+);
 
-    $stringWithData = $email . $delimeter . $password;
+INSERT INTO faculties (faculty_id, faculty_name)
+VALUES
+(1, 'Computer Science'),
+(2, 'Business Administration'),
+(3, 'Engineering');
 
-    $fileToFill = fopen(filename: "data.txt",mode: "a+") or die("Couldn`t read the file");
-    fwrite($fileToFill, $stringWithData . PHP_EOL);
-    fclose($fileToFill);
-}
 
-$fileToFill = fopen(filename: "data.txt",mode: "w+") or die("Couldn`t read the file");
-fclose($fileToFill);
 
-$valueToDelete = 'John Smith';
-$fileContent = file_get_contents("data.txt");
-$newContent = str_replace($valueToDelete, '', $fileContent);
-file_put_contents("data.txt", $newContent);
-echo 'Значання John Smith видалено з файлу'
+create table subjects
+(
+subject_id  integer,
+subject_name TEXT
+);
 
-?>
+INSERT INTO subjects (subject_id, subject_name)
+VALUES
+(1, 'Database Management'),
+(2, 'Financial Accounting'),
+(3, 'Mechanical Engineering');
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Bootstrap test</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-</head>
-<body>
-<div class="container">
+create table grades
+(
+grade_id  integer,
+student_id integer,
+subject_id integer,
+grade integer
+);
 
-    <?php
-    var_dump($_SESSION);
-    ?>
-    <br><br><br>
 
-    <?php if (!empty($_POST['email'])) {
-
-    $_SESSION['is auth'] = true;
-    $_SESSION['user'][] = [
-        'email'    => $_POST['email'],
-        'password' => $_POST['password'],
-    ];
-    ?>
-
-    <div class="alert alert-success-alert" role="alert">
-        Ви намагались залогінитись з Email:
-        <?php
-        echo $_POST['email'];
-        ?>
-        <?php } ?>
-        <br><br><br>
-
-        <form method="post">
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-
-                <br>
-
-                <input
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    required
-                >
-
-                <br><br>
-
-            </div>
-            <div class="mb-3">
-                <label
-                    for="exampleInputPassword1"
-                    class="form-label">Password</label>
-
-                <br>
-
-                <input
-                    type="password"
-                    class="form-control"
-                    name="password"
-                    required
-            </div>
-
-            <br><br>
-
-            <button
-                type="submit"
-                class="btn btn-primary">Sign up</button>
-            <?php
-            session_destroy();
-            ?>
-        </form>
-</body>
-</html>
+INSERT INTO grades (grade_id, student_id, subject_id, grade)
+VALUES
+(1, 1, 1, 90),
+(2, 1, 2, 85),
+(3, 2, 1, 78),
+(4, 3, 3, 92);
