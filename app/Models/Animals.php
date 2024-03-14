aefdfa1dd718032e9f32c904918c4a18bcede4f4
Anton Shvydkyi <tohinson91@gmail.com>
2024-03-14 17:00:50 +0300

    версія 2

--- app/Models/Animals.php
+++ app/Models/Animals.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Animals extends Model{
+class Animals extends Model
+{
     use HasFactory;
 }
